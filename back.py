f1446efd0799578316416929fb50de24b5b89b1c
SwarmGost404 <vik.z.c.r@yandex.ru>
2024-10-15 11:01:15 +0300

    Update back.py
    back.py

--- back.py
+++ back.py
@@ -1,3 +1,38 @@
-a = int(input())
-for i in range(a):
-  print('hello word')
+import sqlite3
+import json
+
+con = sqlite3.connect("tutorial.db")
+cur = con.cursor()
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+    userName        text
+                    not null, 
+    userID          integer
+                    not null, 
+    userPass        text
+                    not null
+    )""")
+    
+#code
+def newUsers(nserName, userID, userPass):
+    cur.execute("""INSERT INTO users(userName, userID, userPass) VALUES(?, ?, ?)""", (nserName, userID, userPass))
+    con.commit()
+    return nserName, userID, userPass
+              
+def select_task_by_priority(userID):
+    cur.execute("SELECT * FROM users WHERE priority=?", (userID == userID))
+    rows = cur.fetchall()
+    return rows
+
+                
+
+def dolist(arg):
+    if arg == "newUser":
+        return newUsers(newUser.get("userName"), newUser.get("userID"), newUser.get("userPass"))
+    elif arg == "openAkk":
+        return select_task_by_priority(newUser.get("userID"))
+
+f = open("newUser.json")
+newUser = json.load(f)
+f.close
+print(dolist(newUser.get("do")))
